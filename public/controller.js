6371441bda8a01f8732cad1839b3478ca5dbb5f9
Jeremy Andaya <jeremy.andaya@gmail.com>
2016-01-16 15:42:14 -0500

    added app.get and persons in server.js

--- public/controller.js
+++ public/controller.js
@@ -6,4 +6,32 @@ var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl',['$scope', '$http',
 	function($scope, $http) {
 		console.log("Hello there from controller.js!");
+
+		// $http.get('/contactList')
+		$http.get('/contactList').success(function(response) {
+			console.log("I got the data I requested!!!");
+			$scope.contactList = response;
+		});
+
+		// person1 = {
+		// 	name: 'Hulk',
+		// 	email: 'hulk@avengers.com',
+		// 	number: '(111) 111-1111'
+		// };
+
+		// person2 = {
+		// 	name: 'Tony',
+		// 	email: 'tony@avengers.com',
+		// 	number: '(222) 222-2222'
+		// };
+
+		// person3 = {
+		// 	name: 'Thor',
+		// 	email: 'thor@avengers.com',
+		// 	number: '(333) 333-3333'
+		// };
+
+		// var contactList = [person1, person2, person3];
+		// $scope.contactList = contactList;
+
 	}]);
--- server.js
+++ server.js
@@ -7,5 +7,30 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList', function(req, res){
+	console.log("I received a GET request!");
+
+	person1 = {
+			name: 'Hulk',
+			email: 'hulk@avengers.com',
+			number: '(111) 111-1111'
+		};
+
+		person2 = {
+			name: 'Tony',
+			email: 'tony@avengers.com',
+			number: '(222) 222-2222'
+		};
+
+		person3 = {
+			name: 'Thor',
+			email: 'thor@avengers.com',
+			number: '(333) 333-3333'
+		};
+
+		var contactList = [person1, person2, person3];
+		res.json(contactList);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
